ea1ddada8017c2c60aca89b65b6ed41486551eff
unknown <you@example.com>
2024-03-06 12:20:20 +0200

    costom order url build

--- server.js
+++ server.js
@@ -2,33 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors'); // Import the cors middleware
-
+const cors = require('cors'); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors()); // Add the cors middleware to allow all origins
-
+app.use(cors());
 
 const dataFilePath = path.join(__dirname, 'form_data.json');
 
-// WITH BASE-URL
-// Serve static files from the 'frontend/build' directory
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// Route handler for serving the CustomOrderForm component
 app.get('/custom-order-form', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
-// END WITH BASE-URL
 
-// Endpoint to submit order
 app.post('/api/submit-order', (req, res) => {
-
-  console.log("IN pp.post -- api-submit-order -- req.body:  ", req.body)
-
   const formData = req.body;
 
   try {
@@ -50,7 +40,6 @@ app.post('/api/submit-order', (req, res) => {
   }
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
