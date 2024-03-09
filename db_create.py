67cf74963edace2adaaf1073d52cb285a4796513
unknown <you@example.com>
2024-03-09 16:07:06 +0200

    db_create.py

---
+++ db_create.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or \
+		'postgresql://postgres:postgres@localhost:5432/mg_landingpage'
+
+db = SQLAlchemy(app)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(50))
+    age_group = db.Column(db.String(50))
+    skill_level = db.Column(db.String(50))
+
+if __name__ == '__main__':
+    # Check if the 'Order' table exists
+    existing_tables = db.engine.table_names()
+    if 'orders' not in existing_tables:
+        # Table does not exist, create it
+        db.create_all()
+    else:
+        print("Table 'Order' already exists.")
+
