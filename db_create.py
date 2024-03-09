7209cd4997503aea2724d0a1015a4cc23421d198
unknown <you@example.com>
2024-03-09 18:57:33 +0200

    railway.json file

--- db_create.py
+++ db_create.py
@@ -3,17 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+
+# Configure the database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or \
     'postgresql://postgres:postgres@localhost:5432/mg_landingpage'
 
+# Initialize SQLAlchemy with the Flask app
 db = SQLAlchemy(app)
 
+# Define the database model
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(50))
     age_group = db.Column(db.String(50))
     skill_level = db.Column(db.String(50))
 
+# Check if the 'Order' table exists
+existing_tables = db.engine.table_names()
+if 'orders' not in existing_tables:
+    # Table does not exist, create it
+    db.create_all()
+else:
+    print("Table 'Order' already exists.")
+
+# Start the Gunicorn server
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    app.run()
