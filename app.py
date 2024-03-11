98eca9710353ac4bc11d419c3f8691fc641c718b
unknown <you@example.com>
2024-03-11 08:56:31 +0200

    2 files

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask import Flask
+from db_create import db, Order
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or \
     'postgresql://postgres:postgres@localhost:5432/mg_landingpage'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    subject = db.Column(db.String(50))
-    age_group = db.Column(db.String(50))
-    skill_level = db.Column(db.String(50))
+# This line is not needed anymore since Order class is imported from db_create.py
+# class Order(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     subject = db.Column(db.String(50))
+#     age_group = db.Column(db.String(50))
+#     skill_level = db.Column(db.String(50))
--- db_create.py
+++ db_create.py
@@ -1,19 +1,9 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or \
-    'postgresql://postgres:postgres@localhost:5432/mg_landingpage'
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(50))
     age_group = db.Column(db.String(50))
     skill_level = db.Column(db.String(50))
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
