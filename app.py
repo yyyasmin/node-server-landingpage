6a3e71f7e45dc840f8287f48e93b6d09d30b38d3
unknown <you@example.com>
2024-03-11 12:08:03 +0200

    railway json

--- app.py
+++ app.py
@@ -6,11 +6,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or \
     'postgresql://postgres:postgres@localhost:5432/mg_landingpage'
 
-db.init_app(app)
+print("")
+print("SQLALCHEMY_DATABASE_URI: ", app.config['SQLALCHEMY_DATABASE_URI'])
+print("")
 
-# This line is not needed anymore since Order class is imported from db_create.py
-# class Order(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     subject = db.Column(db.String(50))
-#     age_group = db.Column(db.String(50))
-#     skill_level = db.Column(db.String(50))
+db.init_app(app)
